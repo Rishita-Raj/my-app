d16b0d2182cd9c1e8af547d8b4cb3ab2d8ef4844
Rishita Raj <rishitaraj1855@gmail.com>
2025-11-10 22:57:46 +0530

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+"use client";
+
+
 import { Button } from '@/components/ui/button'
-
-
 
 const page = () => {
   return (
     <Button>
-      
+      click me
     </Button>
   )
 }
